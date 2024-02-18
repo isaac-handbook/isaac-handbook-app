a1563b3283cc90ae18ddc0421cf2e6a6d71f360f
褚一 <chuyi.lzw@alibaba-inc.com>
2024-02-18 19:50:36 +0800

    feat: 通关率

--- src/global_data.ts
+++ src/global_data.ts
@@ -1,11 +1,5 @@
-import { Cloud, EnvironmentType } from 'laf-client-sdk';
-
 const globalData = {
-  cloud: new Cloud({
-    baseUrl: 'https://xi1g8r.laf.run',
-    getAccessToken: () => wx.getStorageSync('access_token'),
-    environment: EnvironmentType.WX_MP,
-  }),
+  /** 转换器的日志 */
   transformerLogs: [],
 };
 
